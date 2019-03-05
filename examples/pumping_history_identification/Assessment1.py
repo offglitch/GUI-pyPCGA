62aa081377d59cd4d01c0ba6487e13e2c3d764cd
Majid Alturki <malturki@dons.usfca.edu>
2019-03-04 17:52:33 -0800

    Create Assessment1.py
    This will serve as a .py file for us to create module 1 UI

---
+++ examples/pumping_history_identification/Assessment1.py
@@ -0,0 +1,9 @@
+import matplotlib
+matplotlib.use("Agg")
+
+import matplotlib.pyplot as plt
+from scipy.io import savemat, loadmat
+import numpy as np
+import drawdown as dd
+from pyPCGA import PCGA
+import math
