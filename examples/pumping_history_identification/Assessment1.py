a6a349b472a2296bdaf24831fc9cba496a3ec385
Majid Alturki <malturki@dons.usfca.edu>
2019-03-20 08:15:44 -0700

    Delete Assessment1.py

--- examples/pumping_history_identification/Assessment1.py
+++
@@ -1,9 +0,0 @@
-import matplotlib
-matplotlib.use("Agg")
-
-import matplotlib.pyplot as plt
-from scipy.io import savemat, loadmat
-import numpy as np
-import drawdown as dd
-from pyPCGA import PCGA
-import math
