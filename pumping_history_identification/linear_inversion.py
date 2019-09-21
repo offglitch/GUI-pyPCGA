9cd80ed99cdbc163141b065f9e29563bf60aafd0
Majid Alturki <malturki@dons.usfca.edu>
2019-09-21 13:52:39 -0700

    split frontend and backend to seperate files

---
+++ pumping_history_identification/linear_inversion.py
@@ -0,0 +1,286 @@
+﻿from main_ui import Ui_MainWindow
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget
+from pyPCGA import PCGA
+
+import matplotlib
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+from scipy.io import savemat, loadmat
+import numpy as np
+import math
+import drawdown as dd
+  
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+
+    def connectSlots(self):
+
+        self.execute_button.clicked.connect(self.execute)
+
+        self.restart_button.clicked.connect(self.restartFunction)
+
+        self.check_button.clicked.connect(self.switchFunction)
+
+        self.s_true_button.clicked.connect(self.openFileNameDialog)
+
+        self.s_init_box.clicked.connect(self.openFileNameDialog)
+
+        self.Obs_button.clicked.connect(self.openFileNameDialog)
+
+
+
+
+
+
+    def switchFunction(self):
+        passedVals = True
+        x0 = int(self.x0_box.toPlainText())
+        lx = int(self.lx_box.toPlainText())
+        lambdax = int(self.precision_label.toPlainText())
+        n_pc = int(self.n_pc_label.toPlainText())
+        prior_std = float(self.r_label.toPlainText())
+        maxiter = int(self.maxiter_label.toPlainText())
+        restol = float(self.restol_label.toPlainText())
+        # check if values are in their correct range
+        if (x0 < 0):
+            passedVals = False
+        if (lx < 0):
+            passedVals = False
+        if (lambdax < 0):
+            passedVals = False
+        if (n_pc < 0 & n_pc > 200):
+            passedVals = False
+        if (prior_std < 0):
+            passedVals = False
+        if (maxiter < 0):
+            passedVals = False
+        if (restol < 0):
+            passedVals = False
+        
+        
+        if(passedVals):
+
+            msg = QtWidgets.QMessageBox()
+
+            msg.setIcon(QtWidgets.QMessageBox.Information)
+
+            msg.setText("Correct!")
+            msg.setInformativeText("Your values are within the correct range.")
+            msg.setWindowTitle("Congratulations")
+            msg.setStandardButtons(QtWidgets.QMessageBox.Ok)
+            retval = msg.exec_()
+            print("value of pressed message box button:", retval)
+        else:
+            
+            msg = QtWidgets.QMessageBox()
+            
+            msg.setIcon(QtWidgets.QMessageBox.Information)
+
+            msg.setText("Error!")
+            msg.setInformativeText("One or more of your values are incorrect. Please check that your values are correct.")
+            msg.setWindowTitle("Incorrect Values")
+            msg.setStandardButtons(QtWidgets.QMessageBox.Ok)
+            retval = msg.exec_()
+            print("value of pressed message box button:", retval)
+        print("Checked")
+
+    def restartFunction(self):
+        global window
+        global app
+
+        msg = QtWidgets.QMessageBox()
+
+        msg.setIcon(QtWidgets.QMessageBox.Information)
+
+        msg.setText("Restart")
+        msg.setInformativeText("Are you sure you want to restart the program?")
+        msg.setWindowTitle("Restart Message")
+        msg.setStandardButtons(QtWidgets.QMessageBox.Ok | QtWidgets.QMessageBox.Cancel)
+        # gets the button value pressed
+        retval = msg.exec_()
+        print("value of pressed message box button:", retval)
+        #if button == "ok"
+        if (retval == 1024):
+            print("Restarted")
+            #restart the program
+            python = sys.executable
+            os.execl(python, python, * sys.argv)
+        print("exited restart")
+
+
+    # function to pop up open file dialog
+    def openFileNameDialog(self):
+        options = QtWidgets.QFileDialog.Options()
+        options |= QtWidgets.QFileDialog.DontUseNativeDialog
+        fileName, _ = QtWidgets.QFileDialog.getOpenFileName(self.s_true_button,"QFileDialog.getOpenFileName()", "","All Files (*);;Text Files (*.txt)", options=options)
+        if fileName:
+            print(fileName)
+
+    def execute(self):
+        value_2 = int(self.x0_box.toPlainText())
+        value = int(self.lx_box.toPlainText())
+        lambdax = int(self.precision_label.toPlainText())
+        n_pc = int(self.n_pc_label.toPlainText())
+        prior_std = float(self.r_label.toPlainText())
+        maxiter = int(self.maxiter_label.toPlainText())
+        restol = float(self.restol_label.toPlainText())
+        # lm = bool(self.lm_check.isChecked())
+        # linesearch= bool(self.line_search.isChecked())
+
+        print("values: ", value, value_2)
+
+        self.plot(value, value_2, n_pc, maxiter, restol, prior_std, lambdax)
+
+
+    def plot(self, lx, x0, n_pc, maxiter, restol, prior_std, lambdax):
+
+        # This is a 1D case, therefore should be used to test the 1D scenario
+        
+        # M1 parameters are: Lx, Ly, Lz, x0, y0, z0, dx, dy, dz, s_true, s_init
+
+        x0 = x0      # M1: Origin of x dimension 
+        Lx = lx   # M1: Total length in the x direction
+        dxx = 0.1   # M1: Discretization (cell length) in the x direction, assumes cells of equal size
+
+        # This simulation is 1D, therefore default to y_origin = z_origin = 0, Ly = Lz = 1, dy = dz = 1
+
+        y0 = 0      # M1: Origin of y dimension 
+        Ly = 1  # M1: Total length in the y direction
+        dyy = 1     # M1: Discretization (cell length) in the y direction, assumes cells of equal size
+
+        z0 = 0      # M1: Origin of y dimension 
+        Lz = 1  # M1: Total length in the y direction
+        dzz = 1     # M1: Discretization (cell length) in the z direction, assumes cells of equal size
+
+        xmin = np.array([x0]) 
+        xmax = np.array([Lx])
+
+        m = int(Lx/dxx + 1) 
+        N = np.array([m])
+
+
+        _translate = QtCore.QCoreApplication.translate
+
+        #self.m_output.setText(_translate("MainWindow", str(m)))
+        #self.n_output.setText(_translate("MainWindow", str(N)))
+
+
+        dx = np.array([dxx])
+        x = np.linspace(xmin, xmax, m)
+        pts = np.copy(x)
+     
+
+        s_true = np.loadtxt('true.txt') # input for file "true.txt" this can be changed to a default directory  
+        obs = np.loadtxt('obs.txt')
+        #obs = []
+
+
+
+        # if(len(obs) != m):
+
+        #    msg = QtWidgets.QMessageBox()
+
+        #    msg.setIcon(QtWidgets.QMessageBox.Information)
+
+        #    msg.setText("Error!")
+        #    msg.setInformativeText("Something went wrong. Please check that your values are correct.")
+        #    msg.setWindowTitle("Error Message")
+        #    msg.setDetailedText("The details are as follows:")
+        #    msg.setStandardButtons(QtWidgets.QMessageBox.Ok | QtWidgets.QMessageBox.Cancel)
+        #   # msg.buttonClicked.connect(msgbtn)
+    
+        #    retval = msg.exec_()
+        #    print("value of pressed message box button:", retval)
+    
+
+        # print("the values in obs are not correct")
+        # s_init, three options (drop down menu) 
+        # option 1: user inputs a constant which gets assigned to variable s_constant 
+
+        s_constant = 1          # M1: User selects constant checkbox from drop down, and inputs number in box 
+        s_init = s_constant * np.ones((m, 1))
+
+        # option 2: s_init automatically calculated using s_true, if s_true provided
+        # # M1: User selects Auto checkbox from drop down, and check is run to see if s_true was provided 
+        print(m)
+        s_init = np.mean(s_true) * np.ones((m, 1)) #M1 file input or constant input
+        # s_init = np.copy(s_true) # you can try with s_true!
+     
+        prior_std = prior_std #Module 4 (R) 
+        lambdax = lambdax
+        prior_cov_scale = np.array([lambdax]) #M4 lambdas, lx, ly, lz
+
+        def kernel(r): return (prior_std ** 2) * np.exp(-r)  # M4Kernel use switch function
+
+   
+        def forward_model(s, parallelization, ncores=None):
+            params = {}
+            model = dd.Model(params)
+    
+            if parallelization:
+              simul_obs = model.run(s, parallelization, ncores)
+            else:
+               simul_obs = model.run(s, parallelization)
+            return simul_obs
+
+        params = {'R': (prior_std) ** 2, 'n_pc': n_pc,
+             'maxiter': maxiter, 'restol': restol,
+             'matvec': 'FFT', 'xmin': xmin, 'xmax': xmax, 'N': N,
+             'prior_std': prior_std, 'prior_cov_scale': prior_cov_scale,
+             'kernel': kernel, 'post_cov': "diag",
+             'precond': True, 'LM': True,
+             'parallel': True, 'linesearch': True,
+             'forward_model_verbose': False, 'verbose': False,
+             'iter_save': True}
+
+        #initialize
+        prob = PCGA(forward_model, s_init, pts, params, s_true, obs)
+
+        #run inversion
+        s_hat, simul_obs, post_diagv, iter_best = prob.Run()
+
+        post_diagv[post_diagv < 0.] = 0.  
+        post_std = np.sqrt(post_diagv)
+        ### PLOTTING FOR 1D MODULE 1 #############
+        
+        # fig = self.fig.add_subplot(111)
+        fig = self.axs[0]
+        fig.plot(x, s_init,'k-',label='initial')
+        fig.plot(x, s_true,'r-',label='true')
+
+
+        fig.set_title('Pumping history')
+        fig.set_xlabel('Time (min)')
+        fig.set_ylabel(r'Q ($m^3$/min)')
+        fig.legend()
+        
+        ### PLOTTING FOR 1D MODULE 2, 3 & 4 #############
+
+        # fig2 = self.fig.add_subplot(221)
+        fig2 = self.axs[1]
+        fig2.plot(x,s_hat,'k-',label='estimated')
+        fig2.plot(x,s_hat + 2.*post_std,'k--',label='95%')
+        fig2.plot(x,s_hat - 2.*post_std,'k--',label='')
+        fig2.plot(x,s_true,'r-',label='true')
+
+
+        fig2.set_title('Pumping history')
+        fig2.set_xlabel('Time (min)')
+        fig2.set_ylabel(r'Q ($m^3$/min)')
+        fig2.legend()
+        self.plotWidget.draw()
+
+import sys
+import os
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+app = QApplication(sys.argv)
+window = QMainWindow()
+ui = Ui_MainWindow()
+ui.setupUi(window)
+
+window.show()
+sys.exit(app.exec_())
--- pumping_history_identification/main_ui.py
+++ pumping_history_identification/main_ui.py
@@ -1,16 +1,4 @@
 ﻿from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QApplication, QWidget
-from pyPCGA import PCGA
-
-import matplotlib
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-matplotlib.use("Agg")
-import matplotlib.pyplot as plt
-from scipy.io import savemat, loadmat
-import numpy as np
-import math
-import drawdown as dd
-
 
 
 class Ui_MainWindow(object):
@@ -57,7 +45,7 @@ class Ui_MainWindow(object):
         #----------------------------------------------
         # added an execute button
         #----------------------------------------------
-        self.execute_button.clicked.connect(self.execute)
+        # self.execute_button.clicked.connect(self.execute)
         #----------------------------------------------
         # restart button
         #----------------------------------------------
@@ -67,7 +55,7 @@ class Ui_MainWindow(object):
         self.restart_button.setFont(font)
         self.restart_button.setObjectName("restart_button")
         self.gridLayout_6.addWidget(self.restart_button, 1, 1, 1, 1)
-        self.restart_button.clicked.connect(self.restartFunction)
+        # self.restart_button.clicked.connect(self.restartFunction)
         #----------------------------------------------
         # check values button
         #----------------------------------------------
@@ -77,7 +65,7 @@ class Ui_MainWindow(object):
         self.check_button.setFont(font)
         self.check_button.setObjectName("check_button")
         self.gridLayout_6.addWidget(self.check_button, 1, 0, 1, 1)
-        self.check_button.clicked.connect(self.switchFunction)
+        # self.check_button.clicked.connect(self.switchFunction)
         #----------------------------------------------
         # Setting object names and sizing for main frame labels
         #----------------------------------------------
@@ -348,6 +336,8 @@ class Ui_MainWindow(object):
         self.s_true_label.setFont(font)
         self.s_true_label.setObjectName("s_true_label")
         self.gridLayout.addWidget(self.s_true_label, 3, 0, 1, 1)
+
+        # 
         self.s_true_button = QtWidgets.QToolButton(self.Module1Frame)
         self.s_true_button.setMinimumSize(QtCore.QSize(130, 25))
         self.s_true_button.setMaximumSize(QtCore.QSize(130, 25))
@@ -356,7 +346,7 @@ class Ui_MainWindow(object):
         self.s_true_button.setAutoRaise(True)
         self.s_true_button.setObjectName("s_true_button")
         self.gridLayout.addWidget(self.s_true_button, 3, 1, 1, 1)
-        self.s_true_button.clicked.connect(self.openFileNameDialog)
+        # self.s_true_button.clicked.connect(self.openFileNameDialog)
         # self.m_output_label = QtWidgets.QLabel(self.Module1Frame)
         # self.m_output_label.setMaximumSize(QtCore.QSize(50, 20))
         # font = QtGui.QFont()
@@ -396,7 +386,7 @@ class Ui_MainWindow(object):
         self.s_init_box.setAutoRaise(True)
         self.s_init_box.setObjectName("s_init_box")
         self.gridLayout.addWidget(self.s_init_box, 4, 1, 1, 1)
-        self.s_init_box.clicked.connect(self.openFileNameDialog)
+        # self.s_init_box.clicked.connect(self.openFileNameDialog)
         self.s_init_label = QtWidgets.QLabel(self.Module1Frame)
         self.s_init_label.setMaximumSize(QtCore.QSize(50, 20))
         font = QtGui.QFont()
@@ -542,7 +532,7 @@ class Ui_MainWindow(object):
         self.Obs_button.setAutoRaise(True)
         self.Obs_button.setObjectName("Obs_button")
         self.gridLayout_3.addWidget(self.Obs_button, 0, 0, 0, 0)
-        self.Obs_button.clicked.connect(self.openFileNameDialog)
+        # self.Obs_button.clicked.connect(self.openFileNameDialog)
         #----------------------------------------------
         # Setting Module 4's labels and boxes
         #----------------------------------------------
@@ -894,253 +884,3 @@ class Ui_MainWindow(object):
         self.actionSave.setText(_translate("MainWindow", "Save"))
         self.actionDownload.setText(_translate("MainWindow", "Download"))
         self.actionImport.setText(_translate("MainWindow", "Import"))
-
-    def switchFunction(self):
-        passedVals = True
-        x0 = int(self.x0_box.toPlainText())
-        lx = int(self.lx_box.toPlainText())
-        lambdax = int(self.precision_label.toPlainText())
-        n_pc = int(self.n_pc_label.toPlainText())
-        prior_std = float(self.r_label.toPlainText())
-        maxiter = int(self.maxiter_label.toPlainText())
-        restol = float(self.restol_label.toPlainText())
-        # check if values are in their correct range
-        if (x0 < 0):
-            passedVals = False
-        if (lx < 0):
-            passedVals = False
-        if (lambdax < 0):
-            passedVals = False
-        if (n_pc < 0 & n_pc > 200):
-            passedVals = False
-        if (prior_std < 0):
-            passedVals = False
-        if (maxiter < 0):
-            passedVals = False
-        if (restol < 0):
-            passedVals = False
-        
-        
-        if(passedVals):
-
-            msg = QtWidgets.QMessageBox()
-
-            msg.setIcon(QtWidgets.QMessageBox.Information)
-
-            msg.setText("Correct!")
-            msg.setInformativeText("Your values are within the correct range.")
-            msg.setWindowTitle("Congratulations")
-            msg.setStandardButtons(QtWidgets.QMessageBox.Ok)
-            retval = msg.exec_()
-            print("value of pressed message box button:", retval)
-        else:
-            
-            msg = QtWidgets.QMessageBox()
-            
-            msg.setIcon(QtWidgets.QMessageBox.Information)
-
-            msg.setText("Error!")
-            msg.setInformativeText("One or more of your values are incorrect. Please check that your values are correct.")
-            msg.setWindowTitle("Incorrect Values")
-            msg.setStandardButtons(QtWidgets.QMessageBox.Ok)
-            retval = msg.exec_()
-            print("value of pressed message box button:", retval)
-        print("Checked")
-
-    def restartFunction(self):
-        global window
-        global app
-
-        msg = QtWidgets.QMessageBox()
-
-        msg.setIcon(QtWidgets.QMessageBox.Information)
-
-        msg.setText("Restart")
-        msg.setInformativeText("Are you sure you want to restart the program?")
-        msg.setWindowTitle("Restart Message")
-        msg.setStandardButtons(QtWidgets.QMessageBox.Ok | QtWidgets.QMessageBox.Cancel)
-        # gets the button value pressed
-        retval = msg.exec_()
-        print("value of pressed message box button:", retval)
-        #if button == "ok"
-        if (retval == 1024):
-            print("Restarted")
-            #restart the program
-            python = sys.executable
-            os.execl(python, python, * sys.argv)
-        print("exited restart")
-
-
-    # function to pop up open file dialog
-    def openFileNameDialog(self):
-        options = QtWidgets.QFileDialog.Options()
-        options |= QtWidgets.QFileDialog.DontUseNativeDialog
-        fileName, _ = QtWidgets.QFileDialog.getOpenFileName(self.s_true_button,"QFileDialog.getOpenFileName()", "","All Files (*);;Text Files (*.txt)", options=options)
-        if fileName:
-            print(fileName)
-
-    def execute(self):
-        value_2 = int(self.x0_box.toPlainText())
-        value = int(self.lx_box.toPlainText())
-        lambdax = int(self.precision_label.toPlainText())
-        n_pc = int(self.n_pc_label.toPlainText())
-        prior_std = float(self.r_label.toPlainText())
-        maxiter = int(self.maxiter_label.toPlainText())
-        restol = float(self.restol_label.toPlainText())
-        # lm = bool(self.lm_check.isChecked())
-        # linesearch= bool(self.line_search.isChecked())
-
-        print("values: ", value, value_2)
-
-        self.plot(value, value_2, n_pc, maxiter, restol, prior_std, lambdax)
-
-
-    def plot(self, lx, x0, n_pc, maxiter, restol, prior_std, lambdax):
-
-        # This is a 1D case, therefore should be used to test the 1D scenario
-        
-        # M1 parameters are: Lx, Ly, Lz, x0, y0, z0, dx, dy, dz, s_true, s_init
-
-        x0 = x0      # M1: Origin of x dimension 
-        Lx = lx   # M1: Total length in the x direction
-        dxx = 0.1   # M1: Discretization (cell length) in the x direction, assumes cells of equal size
-
-        # This simulation is 1D, therefore default to y_origin = z_origin = 0, Ly = Lz = 1, dy = dz = 1
-
-        y0 = 0      # M1: Origin of y dimension 
-        Ly = 1  # M1: Total length in the y direction
-        dyy = 1     # M1: Discretization (cell length) in the y direction, assumes cells of equal size
-
-        z0 = 0      # M1: Origin of y dimension 
-        Lz = 1  # M1: Total length in the y direction
-        dzz = 1     # M1: Discretization (cell length) in the z direction, assumes cells of equal size
-
-        xmin = np.array([x0]) 
-        xmax = np.array([Lx])
-
-        m = int(Lx/dxx + 1) 
-        N = np.array([m])
-
-
-        _translate = QtCore.QCoreApplication.translate
-
-        #self.m_output.setText(_translate("MainWindow", str(m)))
-        #self.n_output.setText(_translate("MainWindow", str(N)))
-
-
-        dx = np.array([dxx])
-        x = np.linspace(xmin, xmax, m)
-        pts = np.copy(x)
-     
-
-        s_true = np.loadtxt('true.txt') # input for file "true.txt" this can be changed to a default directory  
-        obs = np.loadtxt('obs.txt')
-        #obs = []
-
-
-
-        # if(len(obs) != m):
-
-        #    msg = QtWidgets.QMessageBox()
-
-        #    msg.setIcon(QtWidgets.QMessageBox.Information)
-
-        #    msg.setText("Error!")
-        #    msg.setInformativeText("Something went wrong. Please check that your values are correct.")
-        #    msg.setWindowTitle("Error Message")
-        #    msg.setDetailedText("The details are as follows:")
-        #    msg.setStandardButtons(QtWidgets.QMessageBox.Ok | QtWidgets.QMessageBox.Cancel)
-        #   # msg.buttonClicked.connect(msgbtn)
-    
-        #    retval = msg.exec_()
-        #    print("value of pressed message box button:", retval)
-    
-
-        # print("the values in obs are not correct")
-        # s_init, three options (drop down menu) 
-        # option 1: user inputs a constant which gets assigned to variable s_constant 
-
-        s_constant = 1          # M1: User selects constant checkbox from drop down, and inputs number in box 
-        s_init = s_constant * np.ones((m, 1))
-
-        # option 2: s_init automatically calculated using s_true, if s_true provided
-        # # M1: User selects Auto checkbox from drop down, and check is run to see if s_true was provided 
-        print(m)
-        s_init = np.mean(s_true) * np.ones((m, 1)) #M1 file input or constant input
-        # s_init = np.copy(s_true) # you can try with s_true!
-     
-        prior_std = prior_std #Module 4 (R) 
-        lambdax = lambdax
-        prior_cov_scale = np.array([lambdax]) #M4 lambdas, lx, ly, lz
-
-        def kernel(r): return (prior_std ** 2) * np.exp(-r)  # M4Kernel use switch function
-
-   
-        def forward_model(s, parallelization, ncores=None):
-            params = {}
-            model = dd.Model(params)
-    
-            if parallelization:
-              simul_obs = model.run(s, parallelization, ncores)
-            else:
-               simul_obs = model.run(s, parallelization)
-            return simul_obs
-
-        params = {'R': (prior_std) ** 2, 'n_pc': n_pc,
-             'maxiter': maxiter, 'restol': restol,
-             'matvec': 'FFT', 'xmin': xmin, 'xmax': xmax, 'N': N,
-             'prior_std': prior_std, 'prior_cov_scale': prior_cov_scale,
-             'kernel': kernel, 'post_cov': "diag",
-             'precond': True, 'LM': True,
-             'parallel': True, 'linesearch': True,
-             'forward_model_verbose': False, 'verbose': False,
-             'iter_save': True}
-
-        #initialize
-        prob = PCGA(forward_model, s_init, pts, params, s_true, obs)
-
-        #run inversion
-        s_hat, simul_obs, post_diagv, iter_best = prob.Run()
-
-        post_diagv[post_diagv < 0.] = 0.  
-        post_std = np.sqrt(post_diagv)
-        ### PLOTTING FOR 1D MODULE 1 #############
-        
-        # fig = self.fig.add_subplot(111)
-        fig = self.axs[0]
-        fig.plot(x, s_init,'k-',label='initial')
-        fig.plot(x, s_true,'r-',label='true')
-
-
-        fig.set_title('Pumping history')
-        fig.set_xlabel('Time (min)')
-        fig.set_ylabel(r'Q ($m^3$/min)')
-        fig.legend()
-        
-        ### PLOTTING FOR 1D MODULE 2, 3 & 4 #############
-
-        # fig2 = self.fig.add_subplot(221)
-        fig2 = self.axs[1]
-        fig2.plot(x,s_hat,'k-',label='estimated')
-        fig2.plot(x,s_hat + 2.*post_std,'k--',label='95%')
-        fig2.plot(x,s_hat - 2.*post_std,'k--',label='')
-        fig2.plot(x,s_true,'r-',label='true')
-
-
-        fig2.set_title('Pumping history')
-        fig2.set_xlabel('Time (min)')
-        fig2.set_ylabel(r'Q ($m^3$/min)')
-        fig2.legend()
-        self.plotWidget.draw()
-
-import sys
-import os
-from PyQt5.QtWidgets import QApplication, QMainWindow
-
-app = QApplication(sys.argv)
-window = QMainWindow()
-ui = Ui_MainWindow()
-ui.setupUi(window)
-
-window.show()
-sys.exit(app.exec_())
